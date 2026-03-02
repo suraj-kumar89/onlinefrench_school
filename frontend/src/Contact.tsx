a286a630e1e151979d8e2b12d92c4997a897ba8c
suraj-kumar89 <suraj@bluvodigital.com>
2026-03-02 20:26:34 +0530

    Change in content

--- frontend/src/Contact.tsx
+++ frontend/src/Contact.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SaleBanner from "./components/SaleBanner"; // adjust the path if different
+// import SaleBanner from "./components/SaleBanner"; // adjust the path if different
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import FAQ from "./components/faq/FAQ";
@@ -9,7 +9,7 @@ import ContactHero from "./components/contact/ContactHero";
 const Home: React.FC = () => {
   return (
     <>
-        <SaleBanner endTime="2025-11-13T23:59:00+05:30"  />
+        {/* <SaleBanner endTime="2025-11-13T23:59:00+05:30"  /> */}
         <Header onCtaClick={() => window.location.assign("#book")} />
         <ContactHero/>
         <Journey />
